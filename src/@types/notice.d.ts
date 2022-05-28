ac1b8a9853021ce673877bb1ba7ddd34c68ea8d6
Gura <junpei.m@itnav.co.jp>
2022-05-29 08:57:51 +0900

    add: types

---
+++ src/@types/notice.d.ts
@@ -0,0 +1,8 @@
+export type Notice = {
+  id: string;
+  created_at: string;
+  content: string;
+  user_id: string | undefined;
+};
+
+export type EditedNotice = Omit<Notice, 'created_at' | 'user_id'>;
---
+++ src/@types/task.d.ts
@@ -0,0 +1,8 @@
+export type Task = {
+  id: string;
+  created_at: string;
+  title: string;
+  user_id: string | undefined;
+};
+
+export type EditedTask = Omit<Task, 'created_at' | 'user_id'>;
